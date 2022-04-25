e562e7233d7611aefe477c36dbf758724f5b54af
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-04-25 12:44:07 +0000

    added broken if statements

--- js/script.js
+++ js/script.js
@@ -16,6 +16,18 @@ function calculateWeek () {
 
   // output
   document.getElementById('answers').innerHTML = 'In one week, you listen to ' + week.toFixed(2) + ' hours of music'
+
+  //Dividing as per the weekly music time
+  if (week < 3.00)
+    result = "<br>Hmm not too much!";
+ 
+  else if (bmi >= 18.6 && bmi < 24.9)
+     result = "<br> You're Normal Weight!!";
+ 
+  else if (24.9 < bmi)
+    result = "<br> You're Overweight!!!";
+  else
+    result = "<br> Invalid Answers"
 }
 
 /**
