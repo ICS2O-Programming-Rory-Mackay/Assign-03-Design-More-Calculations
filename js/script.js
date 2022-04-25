5e7087a23990fdcc00baf4a6a7b9ba3dc29c0695
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-04-25 01:37:38 +0000

    added second button to fix js

--- js/script.js
+++ js/script.js
@@ -23,11 +23,14 @@ function calculateWeek () {
  */
 function calculateYear () {
   // input
-  let kilos = parseFloat(document.getElementById('inputKilos').value)
+  let years = parseFloat(document.getElementById('inputAge').value)
 
+  // input
+  let minutes = parseFloat(document.getElementById('inputMinutes').value)
+  
   // process
-  let pounds = kilos * 2.20462262
+  let lifetime = (minutes / 60) * 365 * years
 
   // output
-  document.getElementById('answers2').innerHTML = 'The amount of kilos in pounds is ' + pounds.toFixed(2) + ' pounds'
+  document.getElementById('answers2').innerHTML = 'At this pace, you have listened to ' + lifetime.toFixed(2) + ' hours of music in your lifetime'
 }
